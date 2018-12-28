340bc0b6fa245beced16056874056941ed4b4975
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-28 18:32:14 -0500

    Switch to pure SQLAlchemy instead of Gino

--- server/scripts/initdb.py
+++ server/scripts/initdb.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../')
+
+import asyncio
 from gino import Gino
 
 db = Gino()
@@ -6,10 +10,10 @@ from db.models import Admin
 from db.models import User
 
 async def createDb():
-    async with db.with_bind('postgresql://localhost/datagramDb'):
-        await db.gino.create_all()
-        await User.create(fullname="Hasan Jafri")
-        print(await User.query.gino.all())
+    await db.set_bind('postgresql://postgres:admin3@localhost:5432/datagramDb')
+    await db.gino.create_all()
+    await User.create(fullname="Hasan Jafri")
+    print(await User.query.gino.all())
 
 if __name__ == "__main__":
-    createDb()
+    asyncio.get_event_loop().run_until_complete(createDb())
