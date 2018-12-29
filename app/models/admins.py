c4d246d4802e6578bfbdbaa60caa9568c6a13340
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-29 01:43:03 -0500

    Add admin and project db models

---
+++ app/models/admins.py
@@ -0,0 +1,35 @@
+""" Module represents an Admin. """
+
+from sqlalchemy import (
+    Column, String, Integer,
+    DateTime, Date, Boolean
+)
+
+from app.models import Base
+
+class Admin(Base):
+    __tablename__ = 'admins'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+
+    email = Column(String(255), nullable=False)
+    token_expires = Column(DateTime, nullable=True)
+    perishable_token = Column(String(255), nullable=True, unique=True)
+
+    birthday = Column(Date, nullable=True)
+    first_name = Column(String(35), nullable=True)
+    last_name = Column(String(35), nullable=True)
+
+    is_active = Column(Boolean, default=False)
+
+    def __repr__(self):
+        """ Show admin object info. """
+        return '<Admin: {}>'.format(self.id)
+
+    def full_name(self):
+        """ Return admin's full name. """
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def formatted_birthday(self):
+        """ Return birthday date in a understandable format. """
+        return self.birthday.strftime('%m/%d/%Y')
