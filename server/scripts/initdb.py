37b5cb8b3451793791be52dd3afe9ff70d9daca8
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-28 19:11:19 -0500

    Use pure sqlalchemy in initdb script instead of Gino

--- server/scripts/initdb.py
+++ server/scripts/initdb.py
@@ -1,19 +1,18 @@
 import sys
 sys.path.append('../')
 
-import asyncio
-from gino import Gino
-
-db = Gino()
+from sqlalchemy import create_engine
 
 from db.models import Admin
+from db.models import Base
 from db.models import User
 
-async def createDb():
-    await db.set_bind('postgresql://postgres:admin3@localhost:5432/datagramDb')
-    await db.gino.create_all()
-    await User.create(fullname="Hasan Jafri")
-    print(await User.query.gino.all())
+def createDb():
+    db_engine = create_engine('postgresql://postgres:admin3@localhost:5432/datagramDb', echo=True, connect_args={'connect_timeout': 10})
+    try:
+        Base.metadata.create_all(db_engine)
+    except Exception:
+        raise
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(createDb())
+    createDb()
