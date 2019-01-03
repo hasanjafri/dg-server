c2b3386101d4ef84e0b2774c1470cadd0fe105f0
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-02 22:53:58 -0500

    WIP: Use sessions with redis

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,27 @@
 """ App entry point. """
+import aioredis
 import os
+
 from sanic import Sanic
 from sanic_cors import CORS
+from sanic_session import Session, AIORedisSessionInterface
 
 def create_app():
     """ Function for bootstrapping sanic app. """
 
     app = Sanic(__name__)
+    session = Session()
     CORS(app, automatic_options=True)
 
     # Register Blueprints/Views.
     from app.controllers.users import UserController
     from app.controllers.admins import AdminController
     from app.controllers.projects import ProjectController
+
+    @app.listener('before_server_start')
+    async def server_init(app, loop):
+        app.redis = await aioredis.create_redis_pool(os.environ.get('REDIS_URL'))
+        session.init_app(app, interface=AIORedisSessionInterface(app.redis))
 
     app.add_route(UserController.as_view(), '/api/user')
     app.add_route(AdminController.as_view(), '/api/admin')
