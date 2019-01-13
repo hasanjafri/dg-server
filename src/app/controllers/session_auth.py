a1146151b9cdf3be22f6d75d4074e27548e48480
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-12 20:14:22 -0500

    WIP: SessionAuth Controller

--- src/app/controllers/session_auth.py
+++ src/app/controllers/session_auth.py
@@ -19,5 +19,38 @@ class SessionAuthController(HTTPMethodView):
             return json({'success': 'Authenticated'})
                 
     async def post(self, request):
-        if not request['session'].get('DG_api_key'):
-            return 
+        for param in ['username', 'password', 'account_type']:
+            if request.json.get(param) == None:
+                return json({'error': '{} field cannot be blank'.format(param)})
+
+        username = request.json.get('email')
+        raw_password = request.json.get('password')
+        account_type = request.json.get('account_type')
+
+        user = self.get_user_by_username(account_type, username)
+        if user != None:
+            if check_password(raw_password.encode('utf-8'), user.password_salt, user.password):
+                await self.register_user_login(account_type, username)
+                request['session']['DG_api_key'] = user.api_key
+
+    async def get_user_by_username(self, account_type, username):
+        if account_type == 'user':
+            with scoped_session() as session:
+                user = session.query(User).filter_by(user_name=username).one()
+        elif account_type == 'admin':
+            with scoped_session() as session:
+                user = session.query(Admin).filter_by(email=username).one()
+        
+        return user
+
+    async def register_user_login(self, account_type, username):
+        if account_type == 'user':
+            with scoped_session() as session:
+                session.query(User).filter_by(user_name=username).update({'last_logged_in': datetime.datetime.utcnow()})
+                session.commit()
+        elif account_type == 'admin':
+            with scoped_session() as session:
+                session.query(Admin).filter_by(email=username).update({'last_logged_in': datetime.datetime.utcnow()})
+                session.commit()
+
+        return
