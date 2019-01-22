311fb1f89e3ee4d6a30072ae4eb997395e25297f
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-21 22:37:53 -0500

    Added new model imports in declarative_base init

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -13,3 +13,5 @@ from app.models.admins import Admin
 from app.models.projects import Project
 from app.models.suppliers import Supplier
 from app.models.inventory_products import InventoryProduct
+from app.models.categories import Category
+from app.models.internal_names import InternalName
