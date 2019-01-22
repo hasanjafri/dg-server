dab1a7270c4f9d3f495019bd7266cffbe8c5e639
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-21 22:35:43 -0500

    Updated category and internal name db models

--- src/app/models/categories.py
+++ src/app/models/categories.py
@@ -15,3 +15,8 @@ class Category(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
 
     category_name = Column(String, nullable=False)
+
+    _internal_names = relationship("InternalName", back_populates="category")
+
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    last_updated = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
--- src/app/models/internal_names.py
+++ src/app/models/internal_names.py
@@ -2,7 +2,7 @@ import datetime
 
 from sqlalchemy import (
     Column, Integer, String,
-    DateTime
+    DateTime, ForeignKey
 )
 
 from sqlalchemy.orm import relationship
@@ -17,6 +17,9 @@ class InternalName(Base):
     internal_name = Column(String, nullable=False)
 
     _food_items = relationship('InventoryProduct', back_populates='internal_name')
+
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship('Category', back_populates="_internal_names")
 
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     last_updated = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
